c84c8635d046009900004123ec67e317a5c4ba2a
nzuwera <nzuwera2002@gmail.com>
2019-09-04 15:18:47 +0200

    added pointer usage

--- hello.go
+++ hello.go
@@ -6,6 +6,7 @@ import (
 	"runtime"
 	"reflect"
 )
+
 // Global variables
 var currentOs = runtime.GOOS
 
@@ -18,6 +19,10 @@ func main() {
 func sayHello() {
 	// variable definition inside a function
 	name := "Gilbert"
-	fmt.Println("My name is ",name," is Type of ",reflect.TypeOf(name))
-	fmt.Println("Am using ",currentOs," as OS, which is Type of ",reflect.TypeOf(currentOs))
+	fmt.Println("My name is ", name, " is Type of ", reflect.TypeOf(name))
+	fmt.Println("Am using ", currentOs, " as OS, which is Type of ", reflect.TypeOf(currentOs))
+
+	// Pointers = Memory address of a variable "name"
+	namePointer := &name
+	fmt.Println("This is the momery address of varialbe 'name'", namePointer)
 }
